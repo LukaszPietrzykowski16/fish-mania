34688f27bb9ad116e86b899a0e4a89e5b875471e
unknown <lukas@DESKTOP-JE2377S>
2022-01-05 22:29:21 +0100

    I'm working on opinion section

--- src/Opinion.js
+++ src/Opinion.js
@@ -1,18 +1,55 @@
+import React from 'react';
 import './style.css';
 
 
 
 const Opinion = ()  => {
-  return (
+  const clients = [
+      {
+      id: 0,
+      name: 'Adam Bruh', 
+      opinion: 'loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum'},
+      {
+        id: 1,
+        name: 'Adam Bruh 2', 
+        opinion: 'loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum'
+      }, 
+      {
+        id: 2,
+        name: 'Adam Bruh 3', 
+        opinion: 'loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum'
+      },
+      {
+        id: 3,
+        name: 'Adam Bruh', 
+        opinion: 'loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum'
+      }
+  ]
+
+ const newClient = clients.map((client) => {
+    return (client.name)
+ })
+
+return (
+      
       <div className='opinion-class'>
-          <h1> Opinions </h1>
+          
+          <div className='left'>
+              left
+          </div>
           <div className='clients-opinion'>
-              <h5> Some opinion </h5>
-              <h5> Some opinion 2 </h5>
-              <p> loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum loreem ipsum </p>
+              <h5> {newClient} </h5>
+              
+              <p> </p>
+              
+          </div>
+          <div className='left'>
+              left
           </div>
       </div>
   )
 }
+
+
 
 export default Opinion;
